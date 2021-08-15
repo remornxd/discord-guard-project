a7be7845252e3ea397610398bccb18b9bb82aee6
remornxd <83222811+remornxd@users.noreply.github.com>
2021-08-15 13:50:34 +0300

    Create backup.js
    backup xd?

---
+++ backup.js
@@ -0,0 +1,5 @@
+ya dostum çok anlatılcak bişey yok rolleri kuruyor istediğiniz bottan 30 bot kurun hepsinden rol dağıtsın banane ya neyse bunu gerekli star olunca doldurcam.
+katagoriler silinince guard açıyor.backup botu izinleri ayarlıyor.
+
+çok anlatılcak bir bot değil dostum ya roller silinince sen gelene kadar herşeyi düzeltiyor.
+ben bile canım sıkılınca 20k lık sunucunun rollerini silip kuruyorum sen düşün :d

3c43506111c8cecdfcd6b3ec1b27c8321abf04f7
remornxd <83222811+remornxd@users.noreply.github.com>
2021-11-21 12:52:19 +0300

    Update remornn.js

--- remornn.js
+++ remornn.js
@@ -1,1 +1,9 @@
-X
+const { Client, GuildMember, GuildAuditLogsEntry, WebhookClient, MessageEmbed } = require("discord.js");
+const mongoose = require('mongoose');
+const moment = require("moment");
+const ms = require("ms");
+const RoleData = require("./Models/Roles");
+const ChannelData = require("./Models/Channel");
+const Config = require("./config.json");
+
+XXXXXXXXXXXXXXXXXXXXXX

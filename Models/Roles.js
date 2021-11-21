bc21fbf8d39fd02f6918e3d0f2d2f574f7499bca
remornxd <83222811+remornxd@users.noreply.github.com>
2021-11-21 12:43:09 +0300

    Update Roles.js

--- Models/Roles.js
+++ Models/Roles.js
@@ -1,1 +1,18 @@
-x
+const mongoose = require('mongoose');
+
+const role = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  guildID: String,
+  roleID: String,
+  name: String,
+  color: String,
+  hoist: Boolean,
+  position: Number,
+  permissions: Number,
+  mentionable: Boolean,
+  time: Number,
+  members: Array,
+  channelOverwrites: Array
+});
+
+module.exports = mongoose.model("servername_role_backup", role);
